e304998555a06acdba87bbf9a644c90c8c5e9ad2
Vinay S <vinays@siemens.com>
2020-07-17 22:13:44 +0530

    Added unit tests to cover illegal, allowed illegal, lowercasing the slug scenarios

--- UrlLegalSlug/UrlLegalSlugTests/SlugProducerTests.cs
+++ UrlLegalSlug/UrlLegalSlugTests/SlugProducerTests.cs
@@ -20,5 +20,62 @@ namespace UrlLegalSlugTest
             // Assert
             Assert.AreEqual(expectedSlug, actualSlug, $"We were expecting the Url slug to be: {expectedSlug}, but found the actual Url slug to be {actualSlug}");
         }
+
+        [TestMethod]
+        [TestCategory("ClassTest")]
+        [DataRow("home security access", "home-security-access")]
+        [DataRow("home.security.access", "home-security-access")]
+        [DataRow("home-security-access", "home-security-access")]
+        [DataRow("home–security–access", "home-security-access")]
+        [DataRow("home security–access", "home-security-access")]
+        [DataRow("home -–security–access", "home-security-access")]
+        [DataRow("-home -–security–access-", "home-security-access")]
+
+        public void SlugProducer_GetUrlSlug_WhenTitleContainsAllowedIllegalCharacters_ReplacesAllowedIllegalCharactersWithDash(string inputSlug, string expectedSlugArgument)
+        {
+            //Assert
+            var expectedSlug = expectedSlugArgument;
+
+            // Act
+            var actualSlug = SlugProducer.GetUrlSlug(inputSlug);
+
+            // Assert
+            Assert.AreEqual(expectedSlug, actualSlug, $"We were expecting the Url slug to be: {expectedSlug}, but found the actual Url slug to be {actualSlug}");
+        }
+
+        [TestMethod]
+        [TestCategory("ClassTest")]
+        public void SlugProducer_GetUrlSlug_WhenTitleContainsUpperCaseAlphabets_ReturnsLowerCaseAlphabets()
+        {
+            //Arrange
+            var testSlug = "HoME-sEcuRity-AcCeSS-V1";
+            var expectedSlug = "home-security-access-v1";
+
+            //Act
+            var actualSlug = SlugProducer.GetUrlSlug(testSlug);
+
+            //Assert
+            Assert.AreEqual(expectedSlug, actualSlug, $"We were expecting the Url slug to be: {expectedSlug}, but found the actual Url slug to be {actualSlug}");
+        }
+
+        [TestMethod]
+        [TestCategory("ClassTest")]
+        [DataRow("home@=se!c*\\urity ac`ces$", "homesecurity-acces")]
+        [DataRow("#h^ome security%acc*ess&", "home-securityaccess")]
+        [DataRow("(h>ome s.+curity}-a\":ess)", "home-s-curity-aess")]
+        [DataRow("'h~om?e se+cur<ity}-a\"*ess|", "home-security-aess")]
+        [DataRow("{ho/me-security-acc_ess};", "home-security-access")]
+        public void SlugProducer_GetUrlSlag_WhenTitleContainsIllegalCharacters_ReplacesIllegalCharactersWithBlank(string inputSlug,
+            string expectedSlugArgument)
+        {
+            //Assert
+            var expectedSlug = expectedSlugArgument;
+
+            //Act
+            var actualSlug = SlugProducer.GetUrlSlug(inputSlug);
+
+            //Assert
+            Assert.AreEqual(expectedSlug, actualSlug, $"We were expecting the Url slug to be: {expectedSlug}, but found the actual Url slug to be {actualSlug}");
+        }
     }
 }
